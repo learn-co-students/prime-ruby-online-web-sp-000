76096ad7298cc7560804e4224cdff6cf3f32b8d0
Austin  Burke <aburk3@gmail.com>
2018-10-01 10:51:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+# require 'prime'
+
+# def prime?(int)
+#   int.prime?
+# end
+
+def prime?(int)
+  return false if int <= 1
+  (2..Math.sqrt(int)).none? { |i| (int % i).zero? }
+end
