d0f96fbea909f0d731a401a27196d8403d487851
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-25 21:01:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  n = n.abs
+  if n <= 1
+    false
+  elsif n == 2
+    true
+  else
+    divisors = (2..n - 1).to_a
+    !divisors.any? {|i| n % i == 0}
+  end
+
+end
