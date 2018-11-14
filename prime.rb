c7c7604cd572cecfb1baf1fe459bddbd6d837510
Meisuleen Kased <meisuleen@gmail.com>
2018-11-14 03:36:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+def prime?(int) #num passes through the loops till it figures out if it's prime(returns: false) or else: returns true
+  if int <= 1
+    return false
+  end
+  (2..int - 1).each do |num|
+     if int%num == 0
+       return false
+     end
+   end
+   true
+end
