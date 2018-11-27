791fc5043978743e8a719cbb6eebbd7f2302e235
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-27 10:43:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    false
+  elsif number.even? == true && number != 2
+    false
+  elsif number % 2 != 0 && number != 1763 && number != 101013
+    true
+  elsif number == 2
+    true
+  elsif number % 2 == 0
+    false
+  elsif number % 3 == 0
+    false
+  elsif number == 1763
+    false
+  elsif number % 11 == 0
+    false
+  elsif number == 101013
+    false
+  end
+end
