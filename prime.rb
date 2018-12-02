5419babb563a7db1b57d3ea2e91aee6c64b3d101
Daniel Dawson <dannyldawson@gmail.com>
2018-12-02 07:57:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 
+    false 
+  elsif number == 2 
+    true 
+  else
+    i = 2 
+    while i * i <= number do
+      return false if number % i == 0 
+      i += 1 
+    end
+    true
+  end
+end
