2d099d13d96e2d7555af4006620eec1678230160
Jacob Feldman <jacobgfeld@gmail.com>
2018-12-13 12:28:21 -0500

    Makes prime more efficient

--- prime.rb
+++ prime.rb
@@ -1,24 +1,29 @@
-# Add  code here!
-require "pry"
+# require 'benchmark'
+# require 'bigdecimal/math'
 
-# def prime?(number, current = number - 1) booo stack too short
-#   return TRUE if number == 2
-#   return FALSE if number <= 1
-#   return TRUE if current <= 1
-#   if number%current == 0
-#     return FALSE
-#   else
-#     return prime?(number, current - 1)
+# def primeold?(number)
+# return TRUE if number == 2
+# return FALSE if number <= 1
+#   for i in (2..number - 1)
+#     if number%i == 0
+#       return FALSE
+#     end
 #   end
+#   return true
 # end
 
 def prime?(number)
  return TRUE if number == 2
  return FALSE if number <= 1
-  for i in (2..number - 1)
+ return FALSE if number%2 == 0
+  (3..(Math.sqrt(number)+1)).step(2) do |i|
     if number%i == 0
       return FALSE
     end
   end
   return true
 end
+
+# puts "640663963: "
+# puts Benchmark.measure {primeold?(640663963)}
+# puts Benchmark.measure {prime?(640663963)}
