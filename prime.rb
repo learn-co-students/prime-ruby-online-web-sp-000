e53a987da9b9e657b55e7e313f08fe78b732053f
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-12-18 02:34:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  if integer > 1  
+    denominator = 2 
+    while denominator <= (integer / 2)
+      if integer % denominator != 0
+        denominator += 1
+      else 
+        return false
+      end
+    end
+    true
+  else 
+    false
+  end
+end
