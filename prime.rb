41e83b827c246edba49929487e5f4af1e8a6d79e
David Gilbert <dagilbert333@gmail.com>
2019-01-05 14:00:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num >= 2
+    for d in 2..(num - 1)
+      if (num % d) == 0
+        return false
+      end
+    end
+    true
+  else
+    false
+  end
+end
+
