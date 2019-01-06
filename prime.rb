aee39cafa265e1f3271744fca5e303b5f8451281
Tommy Curley <thomas.curley93@gmail.com>
2019-01-06 10:36:19 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(n) 
+  if (n <= 1)
+    return false
+  elsif (n === 2)
+    return true
+  else
+    puts Math.sqrt(n).ceil
+    for i in 2...Math.sqrt(n).ceil+1
+      if ( n%i === 0) 
+        return false
+      end
+    end
+  end
+  true
+end
+
+
