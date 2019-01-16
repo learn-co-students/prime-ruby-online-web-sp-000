c38a24c376ae4f54abbb990cf19370c0d141cad3
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-16 15:59:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  if number > 1
+    if number == 2
+      return true
+    elsif number.even?
+        return false
+    else
+       range = (2...number).to_a #to an array
+       range.none? do |n|
+         number % n == 0  #remander is it equal to 0
+       end
+    end
+  else
+    return false
+  end
+end
