32cf6d0eaf35d6ea2fc15a0bec4ffcd3c14a7897
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-23 06:42:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  array=(2..(integer-1)).to_a
+  if(integer<=1)
+    return false
+  elsif (integer<=3)
+    return true
+  else 
+  return array.all?{|i| integer % i!=0}
+
+end
+end
