d30502a03d6b12c0e5fdc06001f358e31847496b
Corey H <c.l.halicke@gmail.com>
2019-03-11 02:41:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(int)
+  return false if int <= 0 || int == 1
+  return true if int == 2 || int == 3
+  2.upto(int-1) do |div|
+    if int % div == 0 
+      return false
+    end
+    
+  end
+  return true
+end
