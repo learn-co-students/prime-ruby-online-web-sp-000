d1878bd6f39d4a1e54e543408b0c0a7aa1c38ea7
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-18 18:41:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+    if !number.integer?
+      return false
+    elsif number < 2
+      return false
+    elsif number == 2
+      return true
+    else (2..number-1).each {|integer| return false if number % integer == 0}
+      return true
+    end
+end
