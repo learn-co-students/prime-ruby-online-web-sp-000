c730fb16cc125beae83fd83dd6138831eb040773
Yingqi Chen <chanwk18@hotmail.com>
2019-03-27 04:25:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+# Add  code here
+require 'benchmark'
+def prime?(number)
+  if number<2
+     false
+   elsif number%2==0
+     false
+    else
+      i=3
+     while i<number
+       if number%i==0
+         return false
+      end
+     i=i+1
+   end
+   true
+ end
+  end
+
+  
