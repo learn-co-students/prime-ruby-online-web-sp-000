0fa9fcbbd0baaeba3b8a8a46209ba7bd1c8f5fba
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-29 01:44:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(interger)
+  beg = 2
+  if interger > 1
+    range = (beg..interger-1).to_a
+    range.none? do |number|
+      interger % number == 0
+    end
+  else
+    false
+  end
+end
