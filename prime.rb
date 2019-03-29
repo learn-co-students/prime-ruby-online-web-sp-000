b8c28ada58a69fb4a732b8167a046603f3e72e66
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-29 01:51:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(interger)
   beg = 2
   if interger > 1
@@ -5,6 +6,15 @@ def prime?(interger)
     range.none? do |number|
       interger % number == 0
     end
+  else
+    false
+  end
+end
+=end
+
+def prime(number)
+  if number % 1 == number && number % number == 1
+    true
   else
     false
   end
