d43749b4862991b0e46d7927f864d215a22d7c38
Ashley Miller <asm2fe@virginia.edu>
2019-03-31 19:35:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  i = 2
+  if num < 2
+    return false
+  end
+  while i < num
+    if num % i == 0
+      return false
+    else
+      i += 1
+    end
+  end
+  return true
+end
