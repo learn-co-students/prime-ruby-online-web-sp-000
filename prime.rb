5568437c9b2ad8bdf48fdbc41d61076e1d54a1b7
Arthur Song <arth3rs0ng@gmail.com>
2019-04-05 00:32:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number == 2
+    return true
+  else
+    array = (2..(number - 1)).to_a
+    if array.any?{|nums| number % nums == 0}
+      return false
+      
+    else
+      return true
+    end
+  end
+end
