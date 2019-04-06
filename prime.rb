d3d212c7f4ab359ff99c11e1753de7b0fce227eb
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 21:51:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(integer)
+  # Corner cases 
+   if integer <= 1 
+      return false
+    end 
+ 
+  # create array of factors 
+   factors = []
+   i = 2
+   while i < integer - 1 do 
+     if integer % i == 0 
+       factors << i 
+     end 
+     i += 1
+   end 
+   if factors.empty?
+     true 
+   else 
+     false 
+   end 
+end 
+
+
