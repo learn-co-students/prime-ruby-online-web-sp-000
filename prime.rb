23a3152a3c6ca0a5f977c9dadcfe80d9a90420bb
Andrew Bay <daddnbay@yahoo.com>
2019-04-28 22:43:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num < 1 || num == 1
+    return false
+  elsif num > 1
+    for n in 2...(num)
+      if (num % n) == 0
+        return false
+      end
+    end
+  end
+    true
+end
+   
+  
