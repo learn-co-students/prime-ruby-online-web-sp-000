f2a49202d3315d4b712ef5ba454c6a6167dc9139
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-05-16 21:10:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  is_prime = true
+  if num < 2
+    return is_prime = false
+  else
+  for i in 2..num-1 do
+    if num % i== 0
+      is_prime = false
+    end
+  end
+end
+  is_prime
+end
