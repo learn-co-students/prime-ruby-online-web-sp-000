030108a5097569aa2abcfdb83d8f33b6d498bd57
Becky Phan <beckyphan@live.com>
2019-05-29 04:10:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(integer)
+  divisible_by = []
+  
+  if integer <= 1
+    return false 
+  else 
+    (2..integer).each do |number|
+      if integer % number == 0
+        divisible_by.push(number)
+      else 
+        false
+      end
+    end 
+  end
+  
+  if divisible_by.length == 1
+    return true
+  else 
+    return false
+  end 
+end 
