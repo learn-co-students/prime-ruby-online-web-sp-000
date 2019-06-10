0dbbf1f7dc779b1713fd255d225e46266ab5557c
Connor Cowan <ccowan003@comcast.net>
2019-06-10 23:22:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,2 +1,10 @@
 #Takes integer argument and returns a boolean whether the number is a prime or not.
 def prime?(integer)
+  if integer < 0 or integer == 0 or integer == 1
+    return false
+  else
+    (2..integer-1).to_a.all? do |possible_factor|
+      integer % possible_factor != 0
+    end
+  end
+end
