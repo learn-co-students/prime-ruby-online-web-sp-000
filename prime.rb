2714087a0195163236f16b3f4514a5bee4430c36
Eric Clark <eclark0013@gmail.com>
2019-06-13 16:39:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(x)
+  primeness=true
+  if x<2
+    primeness=false
+  else
+  composite_num_check=
+    Array(2..Math.sqrt(x).floor).any? do |divisor|
+      x%divisor == 0
+    end
+  end
+  if composite_num_check
+    primeness=false
+  end
+  primeness
+end
