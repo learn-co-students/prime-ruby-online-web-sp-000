888a2c6197d4bc2a15f25a4539d19161320786cb
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-20 19:55:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+  number = (num * num) % num
+  (2..(number - 1)).each do |n|
+    return false if number % n == 0
+  end
+  true
+end
+
+
+binding.pry
