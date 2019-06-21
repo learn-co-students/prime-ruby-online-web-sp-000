96c9efa95085370e8182295f07229a0fcba0a072
Peter Grant <jgrantps@hotmail.com>
2019-06-21 02:47:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+return false if num < 2 || num == -1
+#return false if num == -1
+(2...num).each do  |i|
+  if (num % i).zero?
+    return false
+  end
+end
+return true
+end
+
+puts prime?(-11)
