c8e7da00cefb11ea2c22b77be1b8ff4a5e01d513
Shawn <shawntennity@gmail.com>
2019-06-24 09:31:06 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  return false if int <= 1 
+  return true if int == 2
+  i = 2
+  while i < int
+    if int % i == 0
+      return false
+      break
+    else
+    i += 1  
+    end
+  end
+  true
+end
