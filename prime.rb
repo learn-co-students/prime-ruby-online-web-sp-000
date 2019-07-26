2672e38b40e5da6ae630f6cbb50296a3ce839be9
Christian Perez <perezchristian1012@gmail.com>
2019-07-26 21:20:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime?(array)
+  if array < 0 || array == 0 || array == 1
+    return false
+  else (2..array-1).each { |int| return false if array % int == 0}
+ true
+end
+end
