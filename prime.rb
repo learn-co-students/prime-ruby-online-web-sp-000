e95c48627e9e3a50909fe5d7b7f409679696b044
Amy Fong <amyhfong@gmail.com>
2019-08-09 19:55:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+    prime_nums = [2, 3, 5, 7, 11, 13]
+    dividers = Array(2..100)
+    if prime_nums.include?(integer)
+      true
+    elsif integer > 1 && integer.odd? && dividers.all? {|num|
+      integer % num != 0
+    }
+    true
+  else
+    false
+  end
+
+end
