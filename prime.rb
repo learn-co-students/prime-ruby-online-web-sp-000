8195bded78a5685db02a98c3a6cfe45e337823eb
Panela Leung <leungpanela@gmail.com>
2019-08-26 20:31:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require "prime"
+
+def prime? (num)
+  if  (num <= 1)
+    return false
+  elsif num <= 3
+    return true
+  else (2..num/2).none? do |x|
+    num % x == 0
+  end
+  end
+end
