49023e5ba0e2edfe205559d5455441db7b68c0d9
Blythe Pollard <blytheepollard@gmail.com>
2019-09-13 15:48:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(value)
+     return false if value < 2
+    (2..value - 1).each do |x|
+        if (value % x) == 0
+            return false
+        end
+    end
+    true
+end
