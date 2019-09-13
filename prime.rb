67bc28a84e4b89a4d3a2bff17544aabd762c379d
Haim D <haim11293@aol.com>
2019-09-13 18:09:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+# prime numbers are only divisible by 1 and itself.
+def prime?(integer)
+  if integer < 0 || integer == 0 || integer == 1
+    return false
+  else
+    (2..integer-1).to_a.all? { |number| integer % number != 0 }
+  end
+end
