050dbe3ab75675733e9fb1e999a41c0b8994a784
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-08 23:36:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
-def prime?(i)
-  if i >= 2
-  elsif i % 2 == 0
-    false
-  else i % 2 != 0
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif (n != 2) && (n.even? == true)
+   return false
   end
+  range = (2...n-1).to_a
+  if range.any?{|x| n % x == 0}
+    return false
+  end
+   return true
 end
-# && i/3   && i/5 && i/7 && i/11
