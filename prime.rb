8dccb9d4f43b4765cf197fa62b77c4df0fd36a67
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-11-06 18:09:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+ 
+  return true if number == 2
+ 
+  x = 2
+ 
+  prime_check = number % x
+ 
+  while prime_check > 0
+    return true if x == number
+    prime_check = number % x
+    x+=1 
+  end
+  return false
+end
+ 
