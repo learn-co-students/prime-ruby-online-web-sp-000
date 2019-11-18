a5a792901c8f9c16a2f6b3a909330e930691055a
Paul Marshall <pmarshal33@gmail.com>
2019-11-18 16:06:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+require 'pry'
+def prime?(prime_num)
+  return false if !prime_num.integer?
+  return false  if prime_num < 2 
+  return true  if prime_num == 2
+
+(2..prime_num-1).each {|int| return false if prime_num % int == 0}
+true
+end
