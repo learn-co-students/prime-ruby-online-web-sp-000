c475f03774580cfafb089c13e39eb04dcf6e66a5
Erin  Rose <errose10@gmail.com>
2019-11-19 02:35:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime? (stuffs)
+  return false if !stuffs.integer?
+  return false if stuffs < 2
+  return true if stuffs == 2
+  (2..stuffs-1).each {|stuff| return false if stuffs % stuff == 0}
+  true
+end
