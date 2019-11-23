b58b7ffac174e975b0997326e39eb3b65127be3e
Jason J Cruz <jaycru06@yahoo.com>
2019-11-23 11:05:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# require 'benchmark'
+# require 'bigdecimal/math'
+
+def prime?(number)
+  number_range = (2..number-1).to_a
+  if number <= 1 
+    return false 
+  else 
+    number_range.none? {|factor| number % factor == 0} 
+  end
+end 
+
+# puts Benchmark.measure { BigMath.PI(10_000) }
