5c938ca88eda0c024bac5860f3c6f4af7b409dff
Mia <tamiapaschall@gmail.com>
2019-12-17 16:17:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+divisor=Array(2...integer)
+  if integer<=1  
+    false
+  elsif divisor.any?{|num| integer%num==0}
+    false
+  else 
+    true
+  end
+end
