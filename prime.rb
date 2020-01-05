91a059fcb0db83bedbc1eece82c87be6aaa269e8
Doak Covington <doakcovington@protonmail.com>
2020-01-05 22:55:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,10 +2,13 @@
 
 #prime method that return true if interger is prime and false if interger isn't prime
 def prime?(integer)
-  if integer < 2 then return false end
+  if integer < 0 || integer == 0 || integer == 1
+    return false
+  end
+
   for i in 2..integer-1 do
     if integer.to_f % i == 0 then return false end
   end
   true
-end
+  end
 end#end of prime? method
