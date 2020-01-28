92b97495510cc50dbae571269ef75c9ec74a02a9
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:34:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
 # Add  code here!
 def prime?(integer) 
-  
+ if integer < 0 || integer == 0 || integer == 1 
+   return false 
+ else (2..integer-1).to_a.all? do |numbers| 
+   integer % numbers != 0 
+   end 
+ end
+end  
