14b0031cfcff86eebc3ac70b471b178f9d6e8e43
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-30 00:39:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+#this does n-2 iterations
+
+def prime?(n)
+  remainders = []
+  if n <= 1
+  false
+  elsif n > 1
+    (2..n-1).each do |num|
+      remainders << n % num
+    end
+    remainders.none? {|r| r == 0}
+  end
+end
+
