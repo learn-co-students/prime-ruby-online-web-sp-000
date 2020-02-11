9d8d7f15cbd1882e2c10f57b743c64b7921b19f1
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-11 22:56:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+
+def prime?(int)
+  initial = 2
+  if int > 1
+    range = (initial..int-1).to_a
+    range.none? do |num_test| 
+      int % num_test == 0
+    end
+  else
+    false
+  end
+end
