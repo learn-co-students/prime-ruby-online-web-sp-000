8a24b68a40823589b542db3c21f20e09cf6f129b
Kirsten Keels <keelsk@gmail.com>
2020-02-24 22:45:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  if integer > 1
+    array = (1..integer).to_a
+    prime = array.select do |number|
+      integer % number == 0
+    end
+    if (prime.length == 2)
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
