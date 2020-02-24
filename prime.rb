a524388232205d8da752f1087218f2238fcb8690
Jake Garcia <bballwiz4@gmail.com>
2020-02-24 23:23:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  if num == 2
+    return true
+  end
+  (2..(num/2)).each do |factor|
+    return false if num % factor == 0
+  end
+  return true
+end
