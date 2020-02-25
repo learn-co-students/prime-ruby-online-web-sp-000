5a7b53d0d65b9dacf35c82ace74e90e39fff86ff
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-25 21:07:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1 
+      return false 
+    else
+      range = (2..number-1).to_a
+      range.all? do |test_num|
+        number % test_num != 0
+        end
+    end
+end
+    
