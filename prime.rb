2e61aeaeeed47130dd89038ffc7f59a62b516739
Alex Romo III <alexromo3rd@gmail.com>
2020-03-01 14:33:52 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    (2..(number - 1)).each do |num|
+      return false if number % num == 0
+    end
+  end
+  true
+end
