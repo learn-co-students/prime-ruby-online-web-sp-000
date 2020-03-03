22a51d30b4a24ab8f824ee0fa55d1c10d989edde
Josh Ellis <joshe523@gmail.com>
2020-03-03 04:02:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
 # Add  code here!
+def prime?(n)
+  return false if n < 2 # catch special case: negatives, 1, and 0
+  return true if n == 2 # catch special case: 2
+  (2..n-1).to_a.each { |int| return false if n % int == 0 }
+  return true
+end
