1108d56f2dfc4bb8b98854fa0a45e25b949d67bd
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-08 05:08:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  #binding.pry
+  if number == 1 || number <= 0
+    false
+  else
+    divisor = 2
+    while divisor * divisor <= number
+      return false if number % divisor == 0
+      divisor += 1
+    end
+    true
+  end
+end
