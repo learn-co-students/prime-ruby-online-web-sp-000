bef053f19002e73215fd5e27a0917e9413582d23
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-26 14:33:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,15 +1,9 @@
 # Build an array of numbers to test if the number is divisible by any of them
 def prime?(number)
-  if number == 0 || number == -1 || number == 1
+  if number <= 0 || number == 1
     return false
-  elsif number > 1
-    my_array = (2..(number)).to_a
-    my_array.each do |factor|
-      return false if number % factor == 0 && number != factor
-    end
-    true
   else
-    my_array = (-2..(number)).to_a
+    my_array = (2..(number)).to_a
     my_array.each do |factor|
       return false if number % factor == 0 && number != factor
     end
