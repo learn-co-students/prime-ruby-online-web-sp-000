e59e9fd4d2d8021eeba3fb08694f6f57466e663c
santiago molinari <santomolinari@gmail.com>
2020-03-31 18:49:52 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,16 @@
 # Add  code here!
-def prime?(num)
-    if num < 0 or num == 0 or num == 1
-      return false
+
+def prime?(number)
+    if number < 0 or number == 0 or number == 1
+         return false
     else
-      (2..num-1).to_a.all? do |possible_factor|
-        num % possible_factor != 0
-      end
+        (2..number-1).to_a.all? do |possible_factor|
+            number % possible_factor != 0
+        end
     end
-  end
+end
+
+
+
 
 
