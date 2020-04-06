12624f861dfc52c67b74eae6d38f86f9fe7e6812
Adam Spencer <aspencer0919@gmail.com>
2020-04-06 15:19:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num.negative? == true || num == 0 || num == 1
+    return false
+  elsif num == 2
+    return true
+  else
+    n = 2
+    while n < num
+      return false if num % n == 0
+      n += 1
+    end
+    true
+  end
+end
