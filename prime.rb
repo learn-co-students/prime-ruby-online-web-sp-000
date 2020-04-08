fdb14e6864b2d423771699314af1ed36d49506e3
Joseph Kao <josephtkao@gmail.com>
2020-04-08 19:51:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(num)
+  if num == 1
+    return false
+  elsif num <= 0
+    return false
+  elsif num <= 3
+    return true
+  elsif (num % 2 == 0 || num % 3 == 0)
+    return false
+  else
+    i = 5
+    while i**2 < num
+      if num % i == 0
+        puts "#{i}"
+        return false
+      end
+      i += 1
+    end
+    true
+  end
+end
+
+prime?(11)
