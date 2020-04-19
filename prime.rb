feb1fb8529b1ecac9f5871e8585e4a8bf9e40eb2
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-19 05:03:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num.negative? == true
+    return false
+  elsif num == 0 || num == 1
+    return false
+  elsif num.negative? == false
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+  end
+  true
+end
