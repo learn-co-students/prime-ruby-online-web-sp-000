cbf684c781e4bc7ffb40b8127c280393fc790137
The-Gentleman <zraja4402@gmail.com>
2020-04-24 10:34:12 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(n)
+    if n <= 1
+        return false 
+    else  
+        (2..n-1).to_a.each do |num|
+            return false if n % num == 0 
+        end
+    end 
+    true  
+end
