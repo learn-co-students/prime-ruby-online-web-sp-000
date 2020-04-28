dfc94367fd9fbf26b564586d8c59c4a620edd155
Jung Lee <lee.jung.s95@gmail.com>
2020-04-28 03:30:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num <= 1 || num == 1
+    return false
+  elsif
+    (2..num - 1).each do |n|
+      if num % n == 0
+        return false
+      end
+    end
+  end
+  true
+end
