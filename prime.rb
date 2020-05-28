f177bc39fe08be46297b1147c970fd9c500f9f9e
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-28 03:52:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  if num <= 0
+    return false
+  elsif num == 1
+    return false
+  elsif num == 2
+    return true
+  else
+      i = 2
+      while i < num
+        if num%i == 0
+          return false
+        end
+        i += 1
+      end
+      return true
+  end
+end
+
+# puts prime?(12)
+# puts Benchmark.measure {}
