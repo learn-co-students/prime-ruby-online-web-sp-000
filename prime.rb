b4783aaf7dfaf4f7377161be8934d46b45414583
ting682 <tchung682@gmail.com>
2020-06-08 10:34:25 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+#require 'pry'
+def prime?(number)
+    if number == 2 || number == 3 
+        return true
+    elsif number <= 1
+        return false
+    else
+        i = 2
+        while i < number do
+            #binding.pry
+            if number % i == 0
+                return false
+            else
+                i += 1
+                next
+            end
+            i += 1
+        end
+        
+    end
+    return true
+end
+puts prime?(11)
