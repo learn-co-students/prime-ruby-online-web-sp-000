b294ec13f9ccda63ab22b2d4b573cdf4850d04dd
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 03:18:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+
+def prime?(n)
+  return true if n == 2
+  return false if n % 2 == 0 || n < 2
+  (3..(n**0.5).floor).step(2) { |i|
+    return false if n % i == 0
+  }
+  true
+end
