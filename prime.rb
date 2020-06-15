bf40bd7be03f8b67365ae04b472adfd31a6f5621
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-15 16:14:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num === 2 || num === 3
+    return true
+  elsif num % 2 == 0
+    return false
+  elsif num <= 1
+    return false
+  else
+    inc = 5
+    while inc < num && num != inc
+      if num % inc === 0
+        return false
+      end
+      inc += 2
+    end
+    if inc === num
+      return true
+    end
+  end
+end
