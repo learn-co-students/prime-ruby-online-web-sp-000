3d3eb2228a899c07f84ac65192ba4f8942c3ff96
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-22 20:23:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+      range = (start..number-1).to_a
+      range.none? do |test_number|
+      number % test_number == 0
+      end
+  else
+    false 
+  end
+end
