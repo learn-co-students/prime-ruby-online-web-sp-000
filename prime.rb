9daf26e9c90c9ee88cb4600e82d6956ba87c28ab
An Pham <anpham148@gmail.com>
2020-06-23 15:06:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+# def prime?(num)
+#   if num == 1 || num == -1
+#     false
+#   elsif num == 2
+#     true
+#   else
+#     i = 2
+#     while i < num
+#       if num % i == 0
+#         false
+#       end
+#       i += 1
+#   end
+#   true
+# end
+# end
+def prime?(n)
+  return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
+end
