85153f1b65ef4467b984b5d9f10b4c08969ebffd
jasonronalddavis <jdavis@c.ringling.edu>
2020-07-01 23:53:05 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+def prime?(int)
+    if int <= 1 || int == 0 || int == 1
+      return false
+    elsif
+      (2..int - 1).each do |i|
+        if int % i == 0
+        return false
+      end
+    end
+  end
+  true  # <= need explanation
+  end
+
+  
