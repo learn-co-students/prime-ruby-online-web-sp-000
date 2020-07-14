ebd05e75f9c81b3f867e8b1b24fd53ca781f62ad
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-14 22:38:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+counter = 2
+if number > 1 
+  range = (counter..number-1).to_a
+  range.none? do |test_num|
+  number % test_num == 0
+end
+  
+else
+  false
+ end
+end
+
+
+
+
+
+# number is prime if only divisible by 1 or that number
