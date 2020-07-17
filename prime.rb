66aa3994f5fed466405ec8eaaf72c2a945de160b
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-17 04:31:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number == 2
+    return true
+  # eliminate even numbers, we know these are not prime
+  elsif number.odd?
+    i = 3
+    # test if number can be divided evenly by any odd number before it
+    while i < number
+      if number % i == 0; return false end
+      i += 2
+    end
+    # return true if number passes all the tests
+    return true
+  else
+    return false
+  end
+end
