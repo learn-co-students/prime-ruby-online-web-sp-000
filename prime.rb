71a289de1abd2a188dd7a3f98599ac694ffda62c
Karim Alomari <karim.alomari@gmail.com>
2020-07-29 14:44:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(integer)
+  if integer > 1
+    (2...integer).each {|n| return false if integer < 2 || integer % n == 0}
+    return true
+  else
+    return false
+  end
+end
