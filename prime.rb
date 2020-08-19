e9b60df2effe96ba76ad3c1291e99baf5ad36458
shacoca <shacoca@yahoo.com>
2020-08-18 17:39:27 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(number)
     if (number < 2) || (number.even? && number != 2)
         is_prime = false
     elsif number != 3
-        test_factors = []
-        (2..((number ** 0.5).ceil)).to_a.select{|n| n.odd?}.each {|factor| test_factors << factor}
+        # test_factors = []
+        # (2..((number ** 0.5).ceil)).to_a.select{|n| n.odd?}.each {|factor| test_factors << factor}
         test_factors = (2..((number ** 0.5).ceil)).to_a.select{|n| n.odd?}
         candidate_factors = test_factors.select {|factor| number % factor == 0}
         if candidate_factors.length > 0 && candidate_factors[0] != number
