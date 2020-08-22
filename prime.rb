65507052d8554966dccd71dfb24963b8a00476f6
John Stassen <john.stassen@gmail.com>
2020-08-22 15:50:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  half_number = number / 2
+  i = 2
+  factors = []
+  while i < half_number + 1
+    if number % i == 0
+      factors << i
+    end
+    i += 1
+  end
+  if number < 2
+    return false
+  elsif factors.length == 0
+    return true
+  else
+    return false
+  end
+end
