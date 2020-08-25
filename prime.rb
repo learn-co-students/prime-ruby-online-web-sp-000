aad1ca53d03deeca7e4bd9ae402cbf2019bc6077
danny galimi <dannygalimi13@aim.com>
2020-08-25 23:46:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(n)
+    if n <= 1
+      return false
+    else 
+        if (2...n).any? { |i| n % i  == 0}
+            return false
+        else
+            return true
+        end 
+    end
+end
