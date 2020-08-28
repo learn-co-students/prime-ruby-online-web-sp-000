1087a8bf6d9d24a2d5cade032a12e97ee26bf419
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-28 17:56:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+   n = 2
+  return false if num <= 1
+  while n < num
+    return false if num % n == 0
+    puts n 
+    puts num
+    n += 1
+  end
+  true
+end
