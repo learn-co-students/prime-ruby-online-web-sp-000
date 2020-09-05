d3e22739eaa1c7b868115a564addb67710f6eb5b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-05 22:35:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  i = 2
+  commuls = []
+  while i < 100
+    if (number.to_i % i == 0 && number.to_i != i) || (number.to_i < 0) || (number.to_i == 1)
+      commuls << number
+      i += 1
+    else i += 1
+    end
+  end
+commuls == []
+end
