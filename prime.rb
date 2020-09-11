c8bd097427423f5e1cd3dbec23890e09844b351b
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-11 20:06:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  return false if !n.integer?
+
+  return false if n < 2
+
+  return true if n == 2
+
+  (2..n-1).each do |int|
+    return false if n % int == 0
+  end
+  true
+end
