75e4174e614b5cc4af17e44d64c1c3007a15960d
Ryan K Burns <ryankburns@gmail.com>
2020-09-24 17:01:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(int)
+  n = 2
+if int.negative?() || int < 2
+  return false
+elsif
+  while n < int
+    return false if int % n == 0
+    n += 1
+  end
+end
+  true
+end
