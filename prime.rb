c0cc619689281e101fee78fe152fd238078a60b2
Alan Tang <giabao_tang@yahoo.com>
2020-09-27 04:04:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+ return false if num <= 1
+  i = 1
+  count = 0 
+  while i <= num 
+    count += 1 if num % i == 0 
+    i += 1 
+  end
+   count == 2 ? true : false
+end  
+      
