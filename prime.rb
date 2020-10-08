c052f69f118bb7594f7a08e72e5a3af6d760c182
Mike Barberry <mikebarberry@protonmail.com>
2020-10-08 03:47:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+      return false
+  else
+  n = 2
+ while n < num
+   return false if num % n == 0
+   n += 1
+ end
+ true
+ end 
+end
+
+prime?(-1)
