8f431285313d0932309854eae605a28a4b51a348
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-15 05:10:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  divisors = [2]
+
+  return false if num <= 1
+  return true if (num === 2) || (num === 3)
+
+  (2..num-1).each { |p| p.odd? ? divisors << p : next}
+  bool_arr = divisors.collect { |q| (num % q === 0) ? false : true }
+  return bool_arr.all?(true)
+end
