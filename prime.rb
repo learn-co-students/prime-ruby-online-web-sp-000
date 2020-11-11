28d0e9845c337ac022f5c716c5c3fb0678724c89
Andrew Eames <ajerxs@gmail.com>
2020-11-11 16:57:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif
+    (2..number - 1).each do |i|
+      if number % i == 0
+        return false
+      end
+    end
+  end
+  true
+end
