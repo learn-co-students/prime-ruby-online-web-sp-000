18c52525c1c1b922abd5489d47a2a9a10d96300f
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 04:59:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 def prime?(num)
-  (2..(|num| - 1)).each do |n|
+  if num <= 1
+    return false
+  else
+  (2..(num - 1)).each do |n|
     return false if num % n == 0
   end
   return true
-
+end
 end
