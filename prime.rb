954b109fd4601680d2344c0ee929e33a74807f2c
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 19:02:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  if number == 2
+    true
+  elsif number < 2 || number % 2 == 0
+    false
+  else
+    testing = 3
+    while testing < number / 2
+      if number % testing == 0
+        return false
+      else
+        testing += 2
+      end
+    end
+    return true
+  end
+end
