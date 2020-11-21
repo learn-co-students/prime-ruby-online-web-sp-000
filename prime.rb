6bc8f0c3e02feb79a8c67e55fab602d0cd31c454
Jess Pesale <jess_pesale@aol.com>
2020-11-21 18:02:04 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+
+def prime?(int)
+    if int <= 1
+        return false
+   else
+         if (2...int).any? { |i| int % i == 0}
+                return false
+            else
+                return true
+        end
+    end
+end
+#  if n <= 1
+#       return false
+#     else 
+#         if (2...n).any? { |i| n % i  == 0}
+#             return false
+#         else
+#             return true
+#         end 
+#     end
+# end
+
+
