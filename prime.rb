887387b72a28e8f7172ce22cd83c7450fdf4062d
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-02 03:54:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,21 +1,7 @@
 def prime? (number)
   number =*(1..102000)
-  non_prime = 0 
-  prime = 0 
-  number.each do |choice|
-    if choice < 0 
-      non_prime << choice 
-    elsif choice % 2 
-      non_prime << choice 
-    elsif choice % 3 
-      non_prime << choice
-      elsif choice % 5 
-      non_prime << choice 
-      elsif choice % 7 
-      non_prime << choice 
-      elsif  % 11 
-      non_prime << choice
-      end 
-    end 
-  each << prime 
-end  
+  return false if !number.is_a? Integer 
+  return false if number < 2
+  return true if number == 2 
+  (2..number-1).each {|int| return true if number %  int == 0}
+  false end 
