4879ec156f48be020af3deaa926c1270df80885d
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-02 00:05:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime? (number)
+  number =*(1..102000)
+  non_prime = 0 
+  prime = 0 
+  number.each do |choice|
+    if choice < 0 
+      non_prime << choice 
+    elsif choice % 2 
+      non_prime << choice 
+    elsif choice % 3 
+      non_prime << choice
+      elsif choice % 5 
+      non_prime << choice 
+      elsif choice % 7 
+      non_prime << choice 
+      elsif  % 11 
+      non_prime << choice
+      end 
+    end 
+  each << prime 
+end  
