a20e940f360e0428a4afa24fa71bee0fb064341c
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-02 04:53:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,3 @@
-# def prime? (number)
-#   number =*(1..102000)
-#   return false if !number.is_a? Integer 
-#   return false if number < 2
-#   return true if number == 2 
-#   (2..number-1).each {|int| return false if number % int == 0}  
-#   true 
-# end
-
 def prime?(value)
   if value <= 1 || value == 0 || value == 1
     return false
