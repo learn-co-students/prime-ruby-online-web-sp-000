8875c3103dbc4e7729b2960fbe689790ae0851ab
Jouse then <jthen93@gmail.com>
2021-02-16 20:46:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number > 1 
+  
+    (2..(number - 1)).each do |num| 
+      if number % num == 0
+        return false
+      end
+    end
+    return true
+  else
+    return false
+  end
+end
