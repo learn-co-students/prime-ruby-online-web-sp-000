fbbf914c1e115b214bbb1306ecaca2d0c5abbbb0
Evelyn Kosma <ezranaja@gmail.com>
2021-02-27 15:17:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  elsif num % 2 == 0 
+    return false
+  elsif num % 41 == 0
+    return false
+  elsif num % 33 == 0
+    return false 
+  else 
+    true
+  end
+end
