def0cc01123da21dd85ae569eaaefc9cd2e59921
Clayon Stewart <c.stewart89@gmail.com>
2021-03-07 20:55:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  number = number.abs
+  if number <= 1
+    return false
+  else
+    (2..(number - 1)).each do |n|
+      if number % n == 0
+        return false
+      end
+    end
+    true
+  end
+end
