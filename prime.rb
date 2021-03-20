3291a0908230eb6054d8cb3758ffaae9e6e30226
Kristyn Little <kristynlittle@frontier.com>
2021-03-20 03:00:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+#def prime_number(num)
+#  if(num).prime? {|divisor| n % divisor == 0}
+#    true
+#  else
+#    false
+#  end
+#end
+
+def prime?(value)
+  if value <= 1 || value == 1 || value == 0
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true
+end
