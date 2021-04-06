710e903483ce7d7e0851eeec5822b0ff5d9d0e53
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-06 23:12:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require "pry"
+
+def prime?(integer)
+  if integer <= 1 
+    return false 
+  elsif integer == 2 
+   return true 
+  else
+   #binding.pry
+    test_range = (2..integer-1).to_a 
+    test_range.none? do |test_number|
+      integer % test_number == 0 
+    end 
+  end
+end
