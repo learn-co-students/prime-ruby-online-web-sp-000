bf7d26cfe19c8b48d72c9856a8160ae26e3a4e36
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-06 23:13:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,9 @@
-require "pry"
-
 def prime?(integer)
   if integer <= 1 
     return false 
   elsif integer == 2 
    return true 
   else
-   #binding.pry
     test_range = (2..integer-1).to_a 
     test_range.none? do |test_number|
       integer % test_number == 0 
