5a246eb8064dba0f27100037f1cd03de1264301f
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-16 17:24:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
-# Add  code here!
+
+require "pry"
+
+
+
+
+#35711
+
+
+
+
+def prime?(i)
+  if i > 1
+    range = [2,3,5,7]
+    range.any? {|num| (i % num) == 0}
+  else
+    false
+  end
+end
+
+
+
+
+
+
+
+
+
+
